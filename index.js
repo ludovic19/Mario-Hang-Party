866f0d51d28fd0eb175db082ed787982311288c7
Elyes MELE <mele.elyes@gmail.com>
2022-12-10 19:23:15 +0100

    add indice life fonctionnality

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
-  const ListeMotFacile = [
-    {mot : "Cheval",
-    indice : "Animal a sabot"},
-    {mot : "Sabre",
-    indice : "Arme tranchante"}
-  ]
+const ListeMotFacile = [
+  { mot: "CHEVAL", indice: "Animal a sabot" },
+  { mot: "SABRE", indice: "Arme tranchante" },
+];
+const btn = document.querySelectorAll(".boutons .btn");
+let nbrEssai = 5;
+
+function restartTouche(){
+    btn.forEach((elem)=>{if(elem.className=="btnactif"){elem.className="btn inactif"}})
+}
+
+function restartMot(){
+    let paragraphe = document.querySelectorAll('.mot')
+    paragraphe.forEach((elem) => elem.remove())
+        ;}
 
 
-  
-  const btn = document.querySelectorAll(".boutons .btn")
-  btn.forEach((elem)=>{
-    elem.addEventListener('click',function (){
-     statut = elem.className  
-     if(statut = "btn inactif"){elem.className = "btnactif"}
-     else{elem.className = "btn inactif"}
-    })
-  })
+btn.forEach((elem) => {
+  elem.addEventListener("click", function () {
+    statut = elem.className;
+    if ((statut = "btn inactif")) {
+      
+      elem.className = "btnactif";
+      
+      const checkLettre = document.querySelectorAll(".mots p");
+      let nbrLettreErreur = 0
+      checkLettre.forEach((element) => {
+        if (element.innerHTML == elem.innerHTML) {element.style.backgroundColor = "White";} 
+        else {nbrLettreErreur++}
+        });
 
-  const btnStart = document.querySelector(".begin")
-  btnStart.addEventListener('click',function (){
-   const sectionStart = document.querySelector(".start")
-   sectionStart.remove()
-   demarrageJeuNiveau1()
-  })
+    if(nbrLettreErreur==checkLettre.length){nbrEssai--}
+      }
+    else {elem.className = "btn inactif"}
+   
+    if(nbrEssai==0){alert("Perdu");demarrageJeuNiveau1()}
+    }); 
+});
 
-  function demarrageJeuNiveau1 (){
-    let nbreAleatoire = Math.floor(Math.random()*ListeMotFacile.length) 
-    let indice = document.querySelector(".indice p")
-    indice.innerHTML = `"${ListeMotFacile[nbreAleatoire].indice}"`
-    let lettreDuMot = ListeMotFacile[nbreAleatoire].mot.split('')
+const btnStart = document.querySelector(".begin");
+btnStart.addEventListener("click", function () {
+  const sectionStart = document.querySelector(".start");
+  sectionStart.remove();
+  demarrageJeuNiveau1();
+});
 
-  }
+function demarrageJeuNiveau1() {
+    restartTouche()
+    restartMot()
+  let nbreAleatoire = Math.floor(Math.random() * ListeMotFacile.length);
+  let indice = document.querySelector(".indice p");
+  indice.innerHTML = `"${ListeMotFacile[nbreAleatoire].indice}"`;
+  let lettreDuMot = ListeMotFacile[nbreAleatoire].mot.split("");
+  lettreDuMot.forEach((elem) => {
+    const lettre = document.createElement("p");
+    document.querySelector(".mots").appendChild(lettre);
+    lettre.innerHTML = elem;
+    lettre.style.backgroundColor = "Black";
+  });
+}
