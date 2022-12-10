804746cf24d8e548029e591172b4533942682dfa
Elyes MELE <mele.elyes@gmail.com>
2022-12-10 17:17:22 +0100

    add start session

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-  const butn = document.querySelectorAll(".boutons .btn")
-  butn.forEach((elem)=>{
+  const ListeMotFacile = [
+    {mot : "Cheval",
+    indice : "Animal a sabot"},
+    {mot : "Sabre",
+    indice : "Arme tranchante"}
+  ]
+
+
+  
+  const btn = document.querySelectorAll(".boutons .btn")
+  btn.forEach((elem)=>{
     elem.addEventListener('click',function (){
         console.log(elem.className)
      test = elem.className  
      if(test = "btn inactif"){elem.className = "btnactif"}
      else{elem.className = "btn inactif"}
     })
+  })
+
+  const btnStart = document.querySelector(".begin")
+  btnStart.addEventListener('click',function (){
+   const sectionStart = document.querySelector(".start")
+   sectionStart.remove()
   })
